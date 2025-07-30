6cdf757c475840389f87b116ed4d1e9d365fd0fd
thomas rousseau <thomas.rousseau19@gmail.com>
2025-07-30 13:46:55 +0200

    r

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""bip bip"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """bip bip"""
+    return float(sum(mxd_lst))

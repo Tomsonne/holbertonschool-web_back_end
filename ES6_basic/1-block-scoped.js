7a8bfcbb8f90535f86b4d62a1368f0105239784d
thomas rousseau <thomas.rousseau19@gmail.com>
2025-08-19 13:07:56 +0200

    r

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+// bip bip
+
+export default function taskBlock(trueOrFalse) {
+  var task = false;
+  var task2 = true;
+
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
+
+  return [task, task2];
+}

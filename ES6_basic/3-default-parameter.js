48c5173c0be3c5ed059238a76ae750591b2730fc
thomas rousseau <thomas.rousseau19@gmail.com>
2025-08-19 13:33:20 +0200

    r

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,5 +1,5 @@
-// bip bip 
+// bip bip
 
-export default function getSumOfHoods(initialNumber, expansion1989=89, expansion2019) {
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }

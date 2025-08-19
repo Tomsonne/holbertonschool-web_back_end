3feb933797a2fbaeb90018989e0b6ccc514014a7
thomas rousseau <thomas.rousseau19@gmail.com>
2025-08-19 13:15:44 +0200

    r

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+// bip bip
+
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}

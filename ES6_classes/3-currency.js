0d642369d7d61b864c140686cb58baab3cb77c9b
thomas rousseau <thomas.rousseau19@gmail.com>
2025-08-21 13:09:32 +0200

    r

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,41 @@
+
+
+export default class HolbertonCourse {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+
+    this._code = code;
+    this._name = name;
+
+    }
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        if (typeof value !== 'string') {
+        throw new TypeError('Name must be a string');
+        }
+        this._name = value;
+    }
+
+    get code() {
+        return this._code;
+    }
+
+    set code(value) {
+        if (typeof value !== 'string') {
+        throw new TypeError('Code must be a string');
+        }
+        this._name = value;
+    }
+
+    
+
+  }
+

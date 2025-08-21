e29b1d7570ba38d1f97a361dd6fca943c12ce50d
thomas rousseau <thomas.rousseau19@gmail.com>
2025-08-21 14:23:35 +0200

    r

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,12 @@
+// bip bip
+
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}

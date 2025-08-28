d405c27355b760f86dd107656e59c078d56de936
thomas rousseau <thomas.rousseau19@gmail.com>
2025-08-28 13:11:39 +0200

    r

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      const { body } = photo;
+      const { firstName, lastName } = user;
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}

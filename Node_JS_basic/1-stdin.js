ea3def4dd8e930ca25f8fa66229c7baa483a7abe
thomas rousseau <thomas.rousseau19@gmail.com>
2025-09-03 11:04:06 +0200

    r

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+  const name = data;
+  process.stdout.write(`Your name is: ${name}`);
+  process.exit(0);
+});
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
